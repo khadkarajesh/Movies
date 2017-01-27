d9f0dc78356e9da15e2c8350780e0188d8d8e223
rajesh khadka <rajeshkhadka1993@gmail.com>
2017-01-27 23:02:12 +0545

    master:add hello world function

--- app/src/main/java/com/rajesh/movies/MovieActivity.kt
+++ app/src/main/java/com/rajesh/movies/MovieActivity.kt
@@ -8,5 +8,11 @@ class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
+        val a = 1
+        var result: Int
+        result = sum(4, 5)
+        println("sum  = ${result}")
     }
+
+    fun sum(a: Int, b: Int) = a + b
 }
